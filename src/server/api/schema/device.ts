cee9203a327c27a9aa803cf67e9e70a34d224a5f
Vitrixxl <vitrice91@gmail.com>
2025-06-18 07:31:57 +0200

    update fix

--- src/server/api/schema/device.ts
+++ src/server/api/schema/device.ts
@@ -1,9 +1,13 @@
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+import { z } from 'zod';
 import { device } from '~/server/db/schema';
 export const createDeviceSchema = createInsertSchema(device).omit({
   id: true,
   createdAt: true,
 });
 export const updateDeviceSchema = createSelectSchema(device).omit({
+  image: true,
   createdAt: true,
+}).extend({
+  image: z.string().nullish(),
 });
